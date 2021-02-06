4fc977a3ad697f3433f44e18ccd5848aa378f417
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2021-02-06 20:24:18 +0545

    add codecoverage

--- src/webdav4/_types.py
+++ src/webdav4/_types.py
@@ -1,4 +1,8 @@
 """Types."""
 # pylint: disable=unused-import
-from httpx import Response as HTTPResponse  # noqa: F401
-from httpx._types import AuthTypes, HeaderTypes, URLTypes  # noqa: F401
+from httpx import Response as HTTPResponse  # noqa: F401, pragma: no cover
+from httpx._types import (  # noqa: F401, pragma: no cover
+    AuthTypes,
+    HeaderTypes,
+    URLTypes,
+)
